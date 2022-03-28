69377cc5d02458951e89050640841c528cd3741a
cuneguines <cuneguines@gmail.com>
2022-03-28 14:19:17 +0100

    nn

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,27 +16,3 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-import pandas as pd
-import pandas_profiling
-import streamlit as st
-
-#from streamlit_gallery.utils import readme
-from streamlit_pandas_profiling import st_profile_report
-from streamlit_pandas_profiling import st_profile_report
-from pandas_profiling import ProfileReport
-
-# DATA_URL = () 
-nrows=10
-data = pd.read_csv("iris.csv")
-lowercase = lambda x: str(x).lower()
-data.rename(lowercase, axis='columns', inplace=True)
-#data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-
-st.subheader('Raw data')
-st.write(data.head(10))
-st.bar_chart(data.head(10))
-pr = ProfileReport(data.head(5))
-
-st_profile_report(pr)
-#print(data.dtypes)
-#print(data.head(10))
